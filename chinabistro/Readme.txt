0394543544c2e3a11bb090f311895b04a0f3fc32
Haris <haris@athago.com>
2022-04-17 22:24:38 +0200

    Creating the navbar for the china bistro project!

---
+++ chinabistro/Readme.txt
@@ -0,0 +1,6 @@
+Thanks for downloading this Simple Theme!
+
+Template Name: Basic Theme
+Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+Author: Haris Papamatthaiou
+License: https://bootstrapmade.com/license/
---
+++ chinabistro/index.php
@@ -0,0 +1,6 @@
+<?php # Script - index.php - I just include things:
+include("includes/themes/simple_theme/head.html");
+include("includes/themes/simple_theme/nav.html");
+include("includes/themes/simple_theme/main.html");
+include("includes/themes/simple_theme/footer.html");
+?>
