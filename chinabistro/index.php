2bcd0441375a70f6bff9328eb69f878e04a0d072
harispapa <harilaos.papamattheou@gmail.com>
2022-04-27 01:47:41 +0300

    Make active link dynamic in navbar

--- chinabistro/index.php
+++ chinabistro/index.php
@@ -1,6 +1,5 @@
 <?php # Script - index.php - I just include things:
 include("includes/themes/simple_theme/head.html");
-include("includes/themes/simple_theme/nav.html");
 
 // Get the action variable from GLOBAL tables POST,GET,
 // if the action is missing on both tables then set default value:
@@ -12,19 +11,24 @@ if ($action === NULL) {
 }
 switch($action){
      case 'home':
+        $activeMenu= 1;
+        include("includes/themes/simple_theme/nav.html");
         include("includes/themes/simple_theme/main.html");
      break;
      case 'menu':
+       $activeMenu= 2;
+       include("includes/themes/simple_theme/nav.html");
        include("includes/themes/simple_theme/menu.html");
      break;
      case 'about':
+       $activeMenu= 3;
+       include("includes/themes/simple_theme/nav.html");
        include("includes/themes/simple_theme/about.html");
      break;
      case 'awards':
+        $activeMenu= 4;
+        include("includes/themes/simple_theme/nav.html");
         include("includes/themes/simple_theme/awards.html");
-     break;
-     case 'specials':
-        include("includes/themes/simple_theme/menu.html");
      break;
 }
 
