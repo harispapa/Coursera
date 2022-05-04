26cc9d1794bf3c9876075bec86d4919ec98c786f
Charilaos Papamatthaiou <haris.papamattheu@gmail.com>
2022-05-04 16:16:01 +0300

    Load main website with ajax javbascript

--- chinabistro/assets/js/custom_lib/custom.js
+++ chinabistro/assets/js/custom_lib/custom.js
@@ -4,3 +4,42 @@ $( function () {
             $('#navList').collapse('hide');
     });
 });
+
+(function(global){
+    let dc = {};
+    let homeMainContent = "includes/themes/simple_theme/inc/main_content.html";
+
+    // Convenience function for inserting innerHtml for 'select'
+    let insertHtml = (selector, html) => {
+        document.querySelector(selector).innerHtml = html;
+    };
+
+    // Show loading icon inside element identified by 'selector'
+    let showLoading = function (selector) {
+        let html = '<div class="text-center"><img src="content/images/480px-Loader.gif" alt="loader image"></div>';
+        insertHtml(selector,html);
+    }
+
+    // On page load (before images or css)
+    document.addEventListener("DOMContentLoaded", function (event) {
+        // On first load, show home view
+        showLoading("#home_page");
+        $ajaxUtils.sendGetRequest(homeMainContent,  (responseText) => {
+            document.querySelector("#home_page").innerHTML = responseText;
+        }, false);
+    });
+
+    global.$dc =dc;
+})(window);
+
+
+// Convenience function for inserting innerHtml for 'select'
+let insertHtml = (selector, html) => {
+    document.querySelector(selector).innerHtml = html;
+};
+
+// Show loading icon inside element identified by 'selector'
+let showLoading = function (selector) {
+    let html = '<div class="text-center"><img src="content/images/480px-Loader.gif" alt="loader image"></div>';
+    insertHtml(selector,html);
+}
